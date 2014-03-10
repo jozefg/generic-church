7cc42e1c3614c417b557729d231d7572b804d460
Danny Gratzer <danny.gratzer@gmail.com>
2014-03-10 03:14:40 -0500

    Some documentation in Data.Church

--- src/Data/Church.hs
+++ src/Data/Church.hs
@@ -8,6 +8,11 @@ import Data.Proxy
 import GHC.Generics
 
 type Church t c = ChurchSum (ToList (StripMeta (Rep t ())) (ListTerm ())) c
+
+-- | Reify a type to it's church equivalent.
+-- The constraints look pretty scary but can mostly be ignored.
+-- The only one the user should think about is @Generic a@. Everything
+-- else Just Work for types created with @deriving Generic@.
 toChurch :: forall a r. 
             (Generic a, GStripMeta (Rep a ()),
              GList (StripMeta (Rep a ())) (ListTerm ()),
@@ -16,6 +21,8 @@ toChurch :: forall a r.
 toChurch = elim p . flip toList (ListTerm :: ListTerm ()) . Just . stripMeta . from'
   where p = Proxy :: Proxy r
 
+-- | Create a value from it's church representation. Again, the constraints should be mostly ignored.
+-- This method may require an explicit signature.
 fromChurch :: forall a. (Generic a, (GBuild (MakePaths (Rep a ()) '[] '[]) (Church a (Rep a ())) (Rep a ())))
               => Church a (Rep a ()) -> a
 fromChurch c = to $ (build p c :: Rep a ())
