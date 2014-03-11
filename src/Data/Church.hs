0d8813dd54b05dbac85a0a5879c6dbce60c537f7
Danny Gratzer <danny.gratzer@gmail.com>
2014-03-11 11:01:31 -0500

    Better documentation for Church

--- src/Data/Church.hs
+++ src/Data/Church.hs
@@ -7,27 +7,33 @@ import Data.Church.Internal.TF
 import Data.Proxy
 import GHC.Generics
 
+-- | This is the central type for this package. Unfortunately, it's
+-- built around type families so it's not so easy to read. A helpful
+-- algorithm for figuring out what the 'Church' of a type @Foo@ is,
+-- 
+--  1. For each constructor, write out it's type signature
+--  2. Replace the @Foo@ at the end of each signature with @c@
+--  3. Join these type signatures together with arrows @(a -> b -> c) -> c -> ...@
+--  4. Append a final @ -> c@ to the end of this
 type Church t c = ChurchSum (ToList (StripMeta (Rep t ())) (ListTerm ())) c
 
 -- | Reify a type to it's church equivalent.
 -- The constraints look pretty scary but can mostly be ignored.
 -- The only one the user should think about is @Generic a@. Everything
 -- else Just Work for types created with @deriving Generic@.
-toChurch :: forall a r. 
-            (Generic a, GStripMeta (Rep a ()),
-             GList (StripMeta (Rep a ())) (ListTerm ()),
-             GChurchSum (ToList (StripMeta (Rep a ())) (ListTerm ())) r) =>
+-- In other words, read this type signature as @
+toChurch :: forall a r. (Generic a, GStripMeta (Rep a ()),
+                         GList (StripMeta (Rep a ())) (ListTerm ()),
+                         GChurchSum (ToList (StripMeta (Rep a ())) (ListTerm ())) r) =>
             a -> Church a r
 toChurch = elim p . flip toList (ListTerm :: ListTerm ()) . Just . stripMeta . from'
   where p = Proxy :: Proxy r
         from' :: Generic a => a -> Rep a ()
         from' = from
 
-
 -- | Create a value from it's church representation. Again, the constraints should be mostly ignored.
 -- This method may require an explicit signature.
-fromChurch :: forall a. (Generic a, (GBuild (MakePaths (Rep a ()) '[] '[]) (Church a (Rep a ())) (Rep a ())))
-              => Church a (Rep a ()) -> a
+fromChurch :: forall a. (Generic a, (GBuild (MakePaths (Rep a ()) '[] '[]) (Church a (Rep a ())) (Rep a ()))) =>
+              Church a (Rep a ()) -> a
 fromChurch c = to $ (build p c :: Rep a ())
-  where p :: Proxy (MakePaths (Rep a ()) '[] '[])
-        p = Proxy
+  where p = Proxy :: Proxy (MakePaths (Rep a ()) '[] '[])
