4343e738d75ce5eb68fb39c01571b1d4c6277148
Danny Gratzer <danny.gratzer@gmail.com>
2014-03-11 20:47:52 -0500

    API now is a nice type class, some hacking for ghc-bugs though

--- src/Data/Church.hs
+++ src/Data/Church.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE ScopedTypeVariables, FlexibleContexts, DataKinds #-}
 {-# LANGUAGE DefaultSignatures                                #-}
-module Data.Church (fromChurch, pToChurch, Church) where
+module Data.Church (Church, ChurchRep(..)) where
 
 import Data.Church.Internal.ToChurch
 import Data.Church.Internal.FromChurch
@@ -12,34 +12,42 @@ import GHC.Generics
 -- built around type families so it's not so easy to read. A helpful
 -- algorithm for figuring out what the 'Church' of a type @Foo@ is,
 -- 
---  1. For each constructor, write out it's type signature
+--  1. For each constructor, write out its type signature
 --  2. Replace the @Foo@ at the end of each signature with @c@
 --  3. Join these type signatures together with arrows @(a -> b -> c) -> c -> ...@
 --  4. Append a final @ -> c@ to the end of this
 type Church t c = ChurchSum (ToList (StripMeta (Rep t ())) (ListTerm ())) c
 
 class ChurchRep a where
-  -- | Reify a type to it's church equivalent.
-  pToChurch  :: Proxy r -> a -> Church a r
-  default pToChurch :: (Generic a, GStripMeta (Rep a ()),
-                        GList (StripMeta (Rep a ())) (ListTerm ()),
-                        GChurchSum (ToList (StripMeta (Rep a ())) (ListTerm ())) r) =>
-            Proxy r -> a -> Church a r
-  pToChurch p = elim p . flip toList (ListTerm :: ListTerm ()) . Just . stripMeta . from'
+  -- | Reify a type to its church representation
+  toChurch :: forall r. ChurchRep a => a -> Church a r
+  toChurch = toChurchHelper (Proxy :: Proxy r)
+
+  -- | Reify a type to it's church equivalent. This is a hack
+  -- to avoid a bug in GHC 7.6 where -XDefaultSignatures and
+  -- -XScopedTypeVariables together will crash. A non-class
+  -- member function 
+  toChurchHelper :: Proxy r -> a -> Church a r
+  default
+    toChurchHelper :: (Generic a, GStripMeta (Rep a ()),
+                       GList (StripMeta (Rep a ())) (ListTerm ()),
+                       GChurchSum (ToList (StripMeta (Rep a ())) (ListTerm ())) r) =>
+                  Proxy r -> a -> Church a r
+  toChurchHelper p = elim p
+                     . flip toList (ListTerm :: ListTerm ())
+                     . Just
+                     . stripMeta . from'
     where from' :: Generic a => a -> Rep a ()
           from' = from
 
-  -- | Create a value from it's church representation. Again, the constraints should be mostly ignored.
+  -- | Create a value from its church representation.
   -- This method may require an explicit signature.
   fromChurch :: Church a (Rep a ()) -> a
-  default fromChurch :: (Generic a,
-                         (GBuild (MakePaths (Rep a ()) '[] '[])
-                          (Church a (Rep a ()))
-                          (Rep a ()))) =>
+  default
+    fromChurch :: (Generic a,
+                   (GBuild (MakePaths (Rep a ()) '[] '[])
+                    (Church a (Rep a ()))
+                    (Rep a ()))) =>
               Church a (Rep a ()) -> a
-  fromChurch c = to $ (build p c :: Rep a ())
+  fromChurch c = to (build p c :: Rep a ())
     where p = Proxy :: Proxy (MakePaths (Rep a ()) '[] '[])
-
-
-
-
