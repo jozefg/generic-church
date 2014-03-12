5b732ddc184195b75d6e19f7b74e39fa7008cdaa
Danny Gratzer <danny.gratzer@gmail.com>
2014-03-11 19:23:47 -0500

    Beginnings to add typeclass for reification

--- src/Data/Church.hs
+++ src/Data/Church.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE ScopedTypeVariables, FlexibleContexts, DataKinds #-}
-module Data.Church (toChurch, fromChurch, Church) where
+{-# LANGUAGE DefaultSignatures                                #-}
+module Data.Church (fromChurch, pToChurch, Church) where
 
 import Data.Church.Internal.ToChurch
 import Data.Church.Internal.FromChurch
@@ -17,23 +18,28 @@ import GHC.Generics
 --  4. Append a final @ -> c@ to the end of this
 type Church t c = ChurchSum (ToList (StripMeta (Rep t ())) (ListTerm ())) c
 
--- | Reify a type to it's church equivalent.
--- The constraints look pretty scary but can mostly be ignored.
--- The only one the user should think about is @Generic a@. Everything
--- else Just Work for types created with @deriving Generic@.
--- In other words, read this type signature as @
-toChurch :: forall a r. (Generic a, GStripMeta (Rep a ()),
-                         GList (StripMeta (Rep a ())) (ListTerm ()),
-                         GChurchSum (ToList (StripMeta (Rep a ())) (ListTerm ())) r) =>
-            a -> Church a r
-toChurch = elim p . flip toList (ListTerm :: ListTerm ()) . Just . stripMeta . from'
-  where p = Proxy :: Proxy r
-        from' :: Generic a => a -> Rep a ()
-        from' = from
+class ChurchRep a where
+  -- | Reify a type to it's church equivalent.
+  pToChurch  :: Proxy r -> a -> Church a r
+  default pToChurch :: (Generic a, GStripMeta (Rep a ()),
+                        GList (StripMeta (Rep a ())) (ListTerm ()),
+                        GChurchSum (ToList (StripMeta (Rep a ())) (ListTerm ())) r) =>
+            Proxy r -> a -> Church a r
+  pToChurch p = elim p . flip toList (ListTerm :: ListTerm ()) . Just . stripMeta . from'
+    where from' :: Generic a => a -> Rep a ()
+          from' = from
 
--- | Create a value from it's church representation. Again, the constraints should be mostly ignored.
--- This method may require an explicit signature.
-fromChurch :: forall a. (Generic a, (GBuild (MakePaths (Rep a ()) '[] '[]) (Church a (Rep a ())) (Rep a ()))) =>
+  -- | Create a value from it's church representation. Again, the constraints should be mostly ignored.
+  -- This method may require an explicit signature.
+  fromChurch :: Church a (Rep a ()) -> a
+  default fromChurch :: (Generic a,
+                         (GBuild (MakePaths (Rep a ()) '[] '[])
+                          (Church a (Rep a ()))
+                          (Rep a ()))) =>
               Church a (Rep a ()) -> a
-fromChurch c = to $ (build p c :: Rep a ())
-  where p = Proxy :: Proxy (MakePaths (Rep a ()) '[] '[])
+  fromChurch c = to $ (build p c :: Rep a ())
+    where p = Proxy :: Proxy (MakePaths (Rep a ()) '[] '[])
+
+
+
+
